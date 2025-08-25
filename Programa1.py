330454f80b6f4821a4350cf726bf0f025e427876
Laptop_DPC\Diego Plascencia <plascenciacamarenad2@gmail.com>
2025-08-25 13:33:56 -0600

    Primera subida de archivos

---
+++ Programa1.py
@@ -0,0 +1,27 @@
+
+# int a = 0;
+
+a = int(input('Escribe un numero: '))
+print(a**2)
+print(a**(1/2))
+
+##operadores aritmeticos##
+
+# +
+
+# -
+
+# *
+
+# ** potencia, con decimales
+
+# / Exacto, con decimales
+
+# // sin decimales
+
+# mod
+
+# and or
+
+#Para sacar un raiz cuadrada de un numero se deve de elevar el numero a la 1/2
+
